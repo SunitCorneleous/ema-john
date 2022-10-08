d704d2cb677eea41e1c1fd529c74f7b06c2cdf7c
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-10-08 15:45:54 +0600

    items contianer height fixed

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./Order.css";
 import { useLoaderData } from "react-router-dom";
 import { removeFromDb } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
